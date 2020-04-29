224bb2142d7153cd56692a9743fdec6a6f63ec55
tecklearn2307@gmail.com <techlearn2307@gmail.com>
2020-04-29 11:07:28 +0000

    validation

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -9,8 +9,8 @@
             {!! Form::model($task, ['route' => 'tasks.store']) !!}
             
                 <div class="form-group">
-                    {!! Form::label('content', 'status:') !!}
-                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                    {!! Form::label('status', 'status:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
                 </div>
         
                 <div class="form-group">
